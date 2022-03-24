e548b0d6d378e621e97aae143d7b231452d16db1
Artwell <amukwazhe@zimtrade.co.zw>
2022-03-24 10:03:02 +0200

    db create

--- database/migrations/2022_03_24_074239_create_products_table.php
+++ database/migrations/2022_03_24_074239_create_products_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateMytablesTable extends Migration
+class CreateProductsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,12 @@ class CreateMytablesTable extends Migration
      */
     public function up()
     {
-        Schema::create('mytables', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('contact_name');
-            $table->string ('email')->unique();
-            $table->string('address')->nullable();
-            $table->foreignId('image_id');
+            $table->string('productname');
+            $table->string('productdesc');
+            $table->string('client_id');
             $table->timestamps();
-
         });
     }
 
@@ -31,6 +29,6 @@ class CreateMytablesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('mytables');
+        Schema::dropIfExists('products');
     }
 }
